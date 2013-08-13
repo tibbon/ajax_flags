b1a34855f5f3b41c5c4887e1a94c0ba0152cc2da
Christine Coulter <christinetcoulter@gmail.com>
2013-08-13 15:58:06 -0400

    Add handlebar scripts and partial to home.js file. Make sure click events work.

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,15 +2,22 @@ var pointer = 0;
 
 
 function populateCountries() {
+  console.log("clicked populate countries");
 }
 
 function populateAll() {
+  console.log("clicked populate all");
 }
 
-
+function addHandlebarScripts() {
+  $("head").append($("<script id='country-template' type='text/x-handlebars-template'><div><p><span class='flag {{abbreviation}}'></span> {{country}} ({{abbreviation}})</p></div></script>"));
+  $("head").append($("<script id='data-template' type='text/x-handlebars-template'>{{#each countries}}{{> country}}{{/each}}</script>"));
+}
 
 // Create the event bindings
 $(document).ready(function() {
+  addHandlebarScripts();
+
   // Demonstrates using a function name as the event handler instead of including the function inside (like we're used to seeing)
   // This is useful when re-binding events (certain events are unbound when clicking on the various buttons)
   $('#populate-button').click(populateCountries);
