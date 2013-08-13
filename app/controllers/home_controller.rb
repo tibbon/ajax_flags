c60e8128f2d79093c5067d78d3edaffc5eaf288e
alicem <alice.mottola@gmail.com>
2013-08-13 18:48:28 -0400

    Gets data from server via Ajax

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,13 @@
 class HomeController < ApplicationController
 
   def index
-    @countries = Country.select([:id, :abbreviation, :name, :north_america]);
+    # @countries = Country.select([:id, :abbreviation, :name, :north_america]);
   end
 
   def countries
-    
+    respond_to do |format|
+      format.json { render json: Country.select([:id, :abbreviation, :name, :north_america]) }
+    end
   end
 
 end
