d38a7aeace9cd80a06e07dad5313e04a8b462c6a
Beryl <beryl.schragger@gmail.com>
2013-08-13 23:15:25 -0400

    Completes most functionality for flag generation.

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,10 +1,40 @@
 var pointer = 0;
+var number;
+var max = pointer + number;
 
 
-function populateCountries() {
+
+function populateCountries(request_number) {
+  number = request_number;
+  max = pointer + request_number;
+  Handlebars.registerPartial("country", $('#country-template').html());
+  var source = $('#country-template').html();
+  var template = Handlebars.compile(source);
+  $.ajax({
+    url: '/',
+    dataType: 'json',
+    type: 'get'
+  }).done(function(data){
+    for(pointer; pointer < max && data.countries.length; pointer++) {
+      var templateHTML = template(data.countries[pointer]);
+      $('#content').append(templateHTML);
+    }
+    max = pointer + number;
+  })
 }
 
 function populateAll() {
+  Handlebars.registerPartial("country", $('#country-template').html());
+  var source = $('#data-template').html();
+  var template = Handlebars.compile(source);
+  $.ajax({
+    url: '/',
+    dataType: 'json',
+    type: 'get'
+  }).done(function(data){
+    var templateHTML = template(data);
+    $('#content').append(templateHTML);
+  });
 }
 
 
@@ -13,7 +43,7 @@ function populateAll() {
 $(document).ready(function() {
   // Demonstrates using a function name as the event handler instead of including the function inside (like we're used to seeing)
   // This is useful when re-binding events (certain events are unbound when clicking on the various buttons)
-  $('#populate-button').click(populateCountries);
+  $('#populate-button').click(populateCountriesClick);
   $('#all-button').click(allButtonClick);
   $('#reset-button').click(function() {
     // this function resets the button and scroll bindings, and sets pointer to 0
@@ -41,6 +71,13 @@ $(document).ready(function() {
     $('#populate-button').unbind('click');
     $('#all-button').unbind('click');
     populateAll();
+  }
+
+  function populateCountriesClick() {
+    $(window).unbind('scroll');
+    $('#all-button').unbind('click');
+    number = parseInt($('#step-input').val());
+    populateCountries(number);
   }
 
 });
