e2b6f8313704f156b31c2f0afacf5bd25ab13beb
ThomasWhyyou <thomas.whyyou@ymail.com>
2013-08-13 20:09:56 -0400

    hw submission

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,17 @@
 class HomeController < ApplicationController
 
   def index
-    @countries = Country.select([:id, :abbreviation, :name, :north_america]);
+    @countries = Country.select([:id, :abbreviation, :name, :north_america, :created_at]);
+    # @countries = Country.all
+
+    respond_to do |format|
+      format.html
+      format.json { render json: {countries: @countries} }
+    end
   end
 
   def countries
-    
+
   end
 
 end
